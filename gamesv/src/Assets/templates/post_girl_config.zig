283502942c5fd7a4b41e10965f04d0e0039202a9
traffic95 <traffic95@xeondev.com>
2026-07-27 13:45:55 +0200

    fix(gamesv): remove Id from PostGirlConfig
    Duplicates started to show up

--- gamesv/src/Assets/templates/post_girl_config.zig
+++ gamesv/src/Assets/templates/post_girl_config.zig
@@ -1,25 +1,7 @@
 pub const entries: []const Template = @import("PostGirlConfigTemplateTb");
 
-pub const Id = Id: {
-    @setEvalBranchQuota(entries.len);
-
-    var names: [entries.len][]const u8 = undefined;
-    var values: [entries.len]u32 = undefined;
-
-    for (entries, &names, &values) |entry, *name, *value| {
-        value.* = entry.id;
-        name.* = entry.name;
-    }
-
-    break :Id @Enum(u32, .exhaustive, &names, &values);
-};
-
 pub const Template = struct {
     id: u32,
     name: []const u8,
     unlock_condition: []const u8,
-
-    pub inline fn getId(t: *const Template) Id {
-        return @enumFromInt(t.id);
-    }
 };
