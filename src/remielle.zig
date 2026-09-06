3cd3b59cc1d2fc0ee49cccbef9679cf714456857
xeon <xeon@xeondev.com>
2026-09-07 02:22:41 +0300

    remielle: remove `cli` namespace

--- src/remielle.zig
+++ src/remielle.zig
@@ -4,7 +4,6 @@ pub const io = @import("io.zig");
 pub const log = @import("log.zig");
 pub const splash = @import("splash.zig");
 pub const args = @import("args.zig");
-pub const cli = @import("cli.zig");
 pub const rsa = @import("rsa.zig");
 pub const prng = @import("prng.zig");
 pub const mem = @import("mem.zig");
