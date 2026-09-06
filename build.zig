a2274862b0172202f733b60e0dff30001578cb57
xeon <xeon@xeondev.com>
2026-09-07 00:56:19 +0300

    build: script overhaul

--- build.zig
+++ build.zig
@@ -1,6 +1,125 @@
 const std = @import("std");
 const Io = std.Io;
 const Build = std.Build;
+const Optimize = std.lang.Optimize;
+const ResolvedTarget = std.Build.ResolvedTarget;
+
+pub fn build(b: *Build) void {
+    // TODO: use b.dependOn* functionality once it's implemented by the build system.
+    b.graph.poisonCache();
+
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const serve_all_exe = b.addExecutable(.{
+        .name = "serve-all",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("build/serve-all.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
+    });
+
+    const steps = .{
+        .install = b.getInstallStep(),
+        .@"test" = b.step("test", "run the tests"),
+        .@"generate-pb" = b.step("generate-pb", "run a struct generation pass on `main.proto`"),
+        .@"serve-dp" = b.step("serve-dp", "start the dispatch server"),
+        .@"serve-sdk" = b.step("serve-sdk", "start the sdk server"),
+        .@"serve-game" = b.step("serve-game", "start the game server"),
+        .@"serve-all" = @"serve-all": {
+            const run = b.addRunArtifact(serve_all_exe);
+            const step = b.step("serve-all", "start dpsv, sdksv, gamesv at once");
+            step.dependOn(&run.step);
+            break :@"serve-all" run;
+        },
+        .ctl = b.step("ctl", "execute the rmctl command"),
+    };
+
+    const remielle_module = configureRemielleModule(b, .{
+        .@"test" = steps.@"test",
+    }, .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const protobuf_comp = configureProtobufCompilation(b, .{
+        .@"generate-pb" = steps.@"generate-pb",
+    }, .{
+        .optimize = optimize,
+    });
+
+    configureDispatchServer(b, .{
+        .install = steps.install,
+        .@"test" = steps.@"test",
+        .@"serve-dp" = steps.@"serve-dp",
+        .@"serve-all" = steps.@"serve-all",
+    }, .{
+        .remielle_module = remielle_module,
+        .target = target,
+        .optimize = optimize,
+    });
+
+    configureSdkServer(b, .{
+        .install = steps.install,
+        .@"test" = steps.@"test",
+        .@"serve-sdk" = steps.@"serve-sdk",
+        .@"serve-all" = steps.@"serve-all",
+    }, .{
+        .remielle_module = remielle_module,
+        .target = target,
+        .optimize = optimize,
+    });
+
+    configureGameServer(b, .{
+        .install = steps.install,
+        .@"test" = steps.@"test",
+        .@"serve-game" = steps.@"serve-game",
+        .@"serve-all" = steps.@"serve-all",
+    }, .{
+        .remielle_module = remielle_module,
+        .protobuf_comp = protobuf_comp,
+        .target = target,
+        .optimize = optimize,
+    });
+
+    configureRemielleCtl(b, .{
+        .install = steps.install,
+        .@"test" = steps.@"test",
+        .ctl = steps.ctl,
+    }, .{
+        .remielle_module = remielle_module,
+        .target = target,
+        .optimize = optimize,
+    });
+}
+
+fn configureRemielleModule(
+    b: *Build,
+    steps: struct {
+        @"test": *Build.Step,
+    },
+    options: struct {
+        optimize: Optimize,
+        target: ResolvedTarget,
+    },
+) *Build.Module {
+    const remielle = b.addModule(
+        "remielle",
+        .{
+            .root_source_file = b.path("src/remielle.zig"),
+            .target = options.target,
+            .optimize = options.optimize,
+        },
+    );
+
+    StaticAsset.addAll(b, remielle, shared_assets);
+
+    const tests = b.addTest(.{ .root_module = remielle });
+    steps.@"test".dependOn(&b.addRunArtifact(tests).step);
+
+    return remielle;
+}
 
 const stable_protos: []const []const u8 = &.{
     "lib/proto/head.proto",
@@ -8,28 +127,26 @@ const stable_protos: []const []const u8 = &.{
     "lib/proto/persistence.proto",
 };
 
-pub fn build(b: *Build) void {
-    // TODO: use b.dependOn* functionality once it's implemented by the build system.
-    b.graph.poisonCache();
+const ProtobufCompilation = struct {
+    compile_main_descriptors: *Build.Step,
+    compile_stable_definitions: *Build.Step,
+};
 
-    const remielle = b.addModule(
-        "remielle",
-        .{ .root_source_file = b.path("src/remielle.zig") },
-    );
-
-    StaticAsset.addAll(b, remielle, shared_assets);
-
-    const io = b.graph.io;
-    const host = b.graph.host;
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
-
-    const rmprotoc = b.addExecutable(.{
-        .name = "rmprotoc",
+fn configureProtobufCompilation(
+    b: *Build,
+    steps: struct {
+        @"generate-pb": *Build.Step,
+    },
+    options: struct {
+        optimize: Optimize,
+    },
+) ProtobufCompilation {
+    const @"compile-proto" = b.addExecutable(.{
+        .name = "compile-proto",
         .root_module = b.createModule(.{
             .root_source_file = b.path("lib/build/compile-proto.zig"),
-            .target = host,
-            .optimize = optimize,
+            .target = b.graph.host,
+            .optimize = options.optimize,
             .single_threaded = true,
         }),
     });
@@ -38,8 +155,8 @@ pub fn build(b: *Build) void {
     const compile_main_descriptors = b.addUpdateSourceFiles();
     const compile_stable_definitions = b.addUpdateSourceFiles();
 
-    if (b.root.access(io, "lib/proto/main.proto", .{ .read = true })) {
-        const rmprotoc_descs_pass = b.addRunArtifact(rmprotoc);
+    if (b.root.access(b.graph.io, "lib/proto/main.proto", .{ .read = true })) {
+        const rmprotoc_descs_pass = b.addRunArtifact(@"compile-proto");
         rmprotoc_descs_pass.expectExitCode(0);
         rmprotoc_descs_pass.addArg("-descriptors");
         rmprotoc_descs_pass.addFileArg(b.path("lib/proto/main.proto"));
@@ -49,7 +166,7 @@ pub fn build(b: *Build) void {
             "src/protobuf/pb.main.desc.zig",
         );
 
-        const rmprotoc_structs_pass = b.addRunArtifact(rmprotoc);
+        const rmprotoc_structs_pass = b.addRunArtifact(@"compile-proto");
         rmprotoc_structs_pass.expectExitCode(0);
         rmprotoc_structs_pass.addArg("-structures");
         rmprotoc_structs_pass.addFileArg(b.path("lib/proto/main.proto"));
@@ -60,8 +177,8 @@ pub fn build(b: *Build) void {
         );
     } else |_| {}
 
-    if (filesReadable(io, b.root, stable_protos)) {
-        const rmprotoc_stable_pass = b.addRunArtifact(rmprotoc);
+    if (filesReadable(b.graph.io, b.root, stable_protos)) {
+        const rmprotoc_stable_pass = b.addRunArtifact(@"compile-proto");
         rmprotoc_stable_pass.expectExitCode(0);
         rmprotoc_stable_pass.addArg("-full");
 
@@ -74,131 +191,181 @@ pub fn build(b: *Build) void {
         );
     }
 
-    const dpsv = b.addExecutable(.{
+    steps.@"generate-pb".dependOn(&compile_main_structs.step);
+
+    return .{
+        .compile_main_descriptors = &compile_main_descriptors.step,
+        .compile_stable_definitions = &compile_stable_definitions.step,
+    };
+}
+
+const dpsv_assets: []const StaticAsset = &.{
+    .asset("config", "dpsv/config.zon"),
+};
+
+fn configureDispatchServer(b: *Build, steps: struct {
+    install: *Build.Step,
+    @"test": *Build.Step,
+    @"serve-dp": *Build.Step,
+    @"serve-all": *Build.Step.Run,
+}, options: struct {
+    remielle_module: *Build.Module,
+    target: ResolvedTarget,
+    optimize: Optimize,
+}) void {
+    const module = b.createModule(.{
+        .root_source_file = b.path("dpsv/src/main.zig"),
+        .imports = &.{
+            .{ .name = "remielle", .module = options.remielle_module },
+        },
+        .target = options.target,
+        .optimize = options.optimize,
+    });
+
+    StaticAsset.addAll(b, module, dpsv_assets);
+
+    const tests = b.addTest(.{ .root_module = module });
+    steps.@"test".dependOn(&b.addRunArtifact(tests).step);
+
+    const exe = b.addExecutable(.{
         .name = "remielle-dpsv",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("dpsv/src/main.zig"),
-            .imports = &.{
-                .{ .name = "remielle", .module = remielle },
-            },
-            .target = target,
-            .optimize = optimize,
-        }),
+        .root_module = module,
     });
 
-    StaticAsset.addAll(b, dpsv.root_module, dpsv_assets);
+    const install = b.addInstallArtifact(exe, .{});
+    steps.install.dependOn(&install.step);
 
-    const sdksv = b.addExecutable(.{
+    const run = b.addRunArtifact(exe);
+    run.addPassthruArgs();
+    steps.@"serve-dp".dependOn(&run.step);
+
+    steps.@"serve-all".addFileArg(exe.getEmittedBin());
+}
+
+const sdksv_assets: []const StaticAsset = &.{
+    .asset("config", "sdksv/config.zon"),
+};
+
+fn configureSdkServer(b: *Build, steps: struct {
+    install: *Build.Step,
+    @"test": *Build.Step,
+    @"serve-sdk": *Build.Step,
+    @"serve-all": *Build.Step.Run,
+}, options: struct {
+    remielle_module: *Build.Module,
+    target: ResolvedTarget,
+    optimize: Optimize,
+}) void {
+    const module = b.createModule(.{
+        .root_source_file = b.path("sdksv/src/main.zig"),
+        .imports = &.{
+            .{ .name = "remielle", .module = options.remielle_module },
+        },
+        .target = options.target,
+        .optimize = options.optimize,
+    });
+
+    StaticAsset.addAll(b, module, sdksv_assets);
+
+    const tests = b.addTest(.{ .root_module = module });
+    steps.@"test".dependOn(&b.addRunArtifact(tests).step);
+
+    const exe = b.addExecutable(.{
         .name = "remielle-sdksv",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("sdksv/src/main.zig"),
-            .imports = &.{
-                .{ .name = "remielle", .module = remielle },
-            },
-            .target = target,
-            .optimize = optimize,
-        }),
+        .root_module = module,
     });
 
-    StaticAsset.addAll(b, sdksv.root_module, sdksv_assets);
+    const install = b.addInstallArtifact(exe, .{});
+    steps.install.dependOn(&install.step);
 
-    const gamesv = b.addExecutable(.{
+    const run = b.addRunArtifact(exe);
+    run.addPassthruArgs();
+    steps.@"serve-sdk".dependOn(&run.step);
+
+    steps.@"serve-all".addFileArg(exe.getEmittedBin());
+}
+
+const gamesv_assets: []const StaticAsset = &.{
+    .asset("config", "gamesv/config.zon"),
+    .asset("initial_xorpad", "gamesv/initial_xorpad.bytes"),
+};
+
+fn configureGameServer(b: *Build, steps: struct {
+    install: *Build.Step,
+    @"test": *Build.Step,
+    @"serve-game": *Build.Step,
+    @"serve-all": *Build.Step.Run,
+}, options: struct {
+    remielle_module: *Build.Module,
+    protobuf_comp: ProtobufCompilation,
+    target: ResolvedTarget,
+    optimize: Optimize,
+}) void {
+    const module = b.createModule(.{
+        .root_source_file = b.path("gamesv/src/main.zig"),
+        .imports = &.{
+            .{ .name = "remielle", .module = options.remielle_module },
+        },
+        .target = options.target,
+        .optimize = options.optimize,
+    });
+
+    StaticAsset.addAll(b, module, gamesv_assets);
+
+    const tests = b.addTest(.{ .root_module = module });
+    tests.step.dependOn(options.protobuf_comp.compile_main_descriptors);
+    tests.step.dependOn(options.protobuf_comp.compile_stable_definitions);
+    steps.@"test".dependOn(&b.addRunArtifact(tests).step);
+
+    const exe = b.addExecutable(.{
         .name = "remielle-gamesv",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("gamesv/src/main.zig"),
-            .imports = &.{
-                .{ .name = "remielle", .module = remielle },
-            },
-            .target = target,
-            .optimize = optimize,
-        }),
+        .root_module = module,
+    });
+    exe.step.dependOn(options.protobuf_comp.compile_main_descriptors);
+    exe.step.dependOn(options.protobuf_comp.compile_stable_definitions);
+
+    const install = b.addInstallArtifact(exe, .{});
+    steps.install.dependOn(&install.step);
+
+    const run = b.addRunArtifact(exe);
+    run.addPassthruArgs();
+    steps.@"serve-game".dependOn(&run.step);
+
+    steps.@"serve-all".addFileArg(exe.getEmittedBin());
+}
+
+fn configureRemielleCtl(b: *Build, steps: struct {
+    install: *Build.Step,
+    @"test": *Build.Step,
+    ctl: *Build.Step,
+}, options: struct {
+    remielle_module: *Build.Module,
+    target: ResolvedTarget,
+    optimize: Optimize,
+}) void {
+    const module = b.createModule(.{
+        .root_source_file = b.path("rmctl/src/main.zig"),
+        .imports = &.{
+            .{ .name = "remielle", .module = options.remielle_module },
+        },
+        .target = options.target,
+        .optimize = options.optimize,
     });
 
-    StaticAsset.addAll(b, gamesv.root_module, gamesv_assets);
-    gamesv.step.dependOn(&compile_main_descriptors.step);
-    gamesv.step.dependOn(&compile_stable_definitions.step);
+    const tests = b.addTest(.{ .root_module = module });
+    steps.@"test".dependOn(&b.addRunArtifact(tests).step);
 
-    const rmctl = b.addExecutable(.{
+    const exe = b.addExecutable(.{
         .name = "rmctl",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("rmctl/src/main.zig"),
-            .imports = &.{
-                .{ .name = "remielle", .module = remielle },
-            },
-            .target = target,
-            .optimize = optimize,
-        }),
+        .root_module = module,
     });
 
-    b.step(
-        "pb",
-        "run a struct generation pass on `main.proto`",
-    ).dependOn(&compile_main_structs.step);
+    const install = b.addInstallArtifact(exe, .{});
+    steps.install.dependOn(&install.step);
 
-    const ctl = b.addRunArtifact(rmctl);
-    const serve_dp = b.addRunArtifact(dpsv);
-    const serve_sdk = b.addRunArtifact(sdksv);
-    const serve_game = b.addRunArtifact(gamesv);
-
-    ctl.addPassthruArgs();
-    serve_dp.addPassthruArgs();
-    serve_sdk.addPassthruArgs();
-    serve_game.addPassthruArgs();
-
-    b.step(
-        "ctl",
-        "execute the rmctl command",
-    ).dependOn(&ctl.step);
-
-    b.step(
-        "serve-dp",
-        "start the dispatch server",
-    ).dependOn(&serve_dp.step);
-
-    b.step(
-        "serve-sdk",
-        "start the sdk server",
-    ).dependOn(&serve_sdk.step);
-
-    b.step(
-        "serve-game",
-        "start the game server",
-    ).dependOn(&serve_game.step);
-
-    b.installArtifact(dpsv);
-    b.installArtifact(sdksv);
-    b.installArtifact(gamesv);
-    b.installArtifact(rmctl);
-
-    const serve_all_exe = b.addExecutable(.{
-        .name = "serve-all",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("build/serve-all.zig"),
-            .target = target,
-            .optimize = optimize,
-        }),
-    });
-
-    const serve_all = b.addRunArtifact(serve_all_exe);
-    serve_all.addFileArg(dpsv.getEmittedBin());
-    serve_all.addFileArg(sdksv.getEmittedBin());
-    serve_all.addFileArg(gamesv.getEmittedBin());
-
-    b.step(
-        "serve-all",
-        "start dpsv, sdksv, gamesv at once",
-    ).dependOn(&serve_all.step);
-
-    const tests = b.addTest(.{
-        .name = "remielle",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/remielle.zig"),
-            .target = target,
-            .optimize = optimize,
-        }),
-    });
-
-    b.step("test", "run unit tests").dependOn(&b.addRunArtifact(tests).step);
+    const run = b.addRunArtifact(exe);
+    run.addPassthruArgs();
+    steps.ctl.dependOn(&run.step);
 }
 
 const shared_assets: []const StaticAsset = &.{
@@ -234,19 +401,6 @@ const shared_assets: []const StaticAsset = &.{
     .asset("interacts.remi", "assets/graphs/interacts.remi"),
 };
 
-const gamesv_assets: []const StaticAsset = &.{
-    .asset("config", "gamesv/config.zon"),
-    .asset("initial_xorpad", "gamesv/initial_xorpad.bytes"),
-};
-
-const dpsv_assets: []const StaticAsset = &.{
-    .asset("config", "dpsv/config.zon"),
-};
-
-const sdksv_assets: []const StaticAsset = &.{
-    .asset("config", "sdksv/config.zon"),
-};
-
 fn filesReadable(io: Io, dir: Build.Cache.Path, path_list: []const []const u8) bool {
     for (path_list) |sub_path|
         dir.access(io, sub_path, .{ .read = true }) catch return false;
@@ -258,7 +412,7 @@ const StaticAsset = struct {
     import_name: []const u8,
     sub_path: []const u8,
 
-    pub inline fn asset(import_name: []const u8, sub_path: []const u8) StaticAsset {
+    pub fn asset(import_name: []const u8, sub_path: []const u8) StaticAsset {
         return .{ .import_name = import_name, .sub_path = sub_path };
     }
 
