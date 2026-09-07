98a673672537240edb256069ac812814bd3130b8
xeon <xeon@xeondev.com>
2026-09-07 13:05:11 +0300

    dpsv: use SafeAllocator in safe modes

--- dpsv/src/main.zig
+++ dpsv/src/main.zig
@@ -1,3 +1,5 @@
+const builtin = @import("builtin");
+
 const std = @import("std");
 const Io = std.Io;
 const net = std.Io.net;
@@ -10,12 +12,19 @@ const Data = @import("Data.zig");
 
 const log = std.log.scoped(.@"remielle-dpsv");
 
-pub const Args = struct {
-    @"--listen-address": []const u8 = @import("config").listen_address,
-};
-
 pub const std_options: std.Options = .{
     .logFn = remielle.log.logFn,
+};
+
+var safe_allocator: std.heap.SafeAllocator = .init(std.heap.page_allocator, .{});
+
+const use_safe_allocator = switch (builtin.optimize) {
+    .debug, .safe => true,
+    .small, .fast => false,
+};
+
+pub const Args = struct {
+    @"--listen-address": []const u8 = @import("config").listen_address,
 };
 
 pub fn usage(io: Io) noreturn {
@@ -32,32 +41,42 @@ pub fn usage(io: Io) noreturn {
     process.exit(0);
 }
 
-pub fn main(init: process.Init.Minimal) void {
-    var arena: std.heap.ArenaAllocator = .init(std.heap.page_allocator);
+fn fatal(comptime fmt: []const u8, args: anytype) noreturn {
+    log.err(fmt, args);
+    std.process.exit(1);
+}
 
-    const args_slice = init.args.toSlice(arena.allocator()) catch |err|
-        fatal("failed to collect cli arguments: {t}", .{err});
+pub fn main(init: process.Init.Minimal) !void {
+    const gpa = if (use_safe_allocator) safe_allocator.allocator() else std.heap.smp_allocator;
+    defer if (use_safe_allocator) {
+        _ = safe_allocator.deinit();
+    };
+
+    var arena_instance: std.heap.ArenaAllocator = .init(std.heap.page_allocator);
+    defer arena_instance.deinit();
+    const arena = arena_instance.allocator();
 
     var io_impl = if (remielle.io.RemiellIo.supported)
-        remielle.io.RemiellIo.init(std.heap.page_allocator, .{
+        remielle.io.RemiellIo.init(gpa, .{
             .coroutine_limit = .unlimited, // TODO
             .stack_size = 1024 * 128,
         }) catch |err|
             fatal("failed to init I/O implementation: {t}", .{err})
     else
-        Io.Threaded.init(std.heap.page_allocator, .{
+        Io.Threaded.init(gpa, .{
             .argv0 = .init(init.args),
             .environ = init.environ,
         });
     defer io_impl.deinit();
     const io = io_impl.io();
 
+    const args_slice = try init.args.toSlice(arena);
     const args = remielle.args.parse(Args, log, args_slice) orelse usage(io);
 
     const listen_address = net.IpAddress.parseLiteral(args.@"--listen-address") catch |err|
         fatal("bad listen address specified: {t}", .{err});
 
-    const data = Data.build(arena.allocator()) catch |err| switch (err) {
+    const data = Data.build(arena) catch |err| switch (err) {
         error.OutOfMemory => fatal("failed to build static responses", .{}),
     };
 
@@ -74,9 +93,4 @@ pub fn main(init: process.Init.Minimal) void {
     } else {
         app_future.await(io) catch {};
     }
-}
-
-inline fn fatal(comptime fmt: []const u8, args: anytype) noreturn {
-    log.err(fmt, args);
-    std.process.exit(1);
 }
