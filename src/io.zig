5aa8471fd6353753eb6c5fb03d0ebeeaa3cdb20f
xeon <xeon@xeondev.com>
2026-09-07 14:17:56 +0300

    io: implement `waitForShutdownThreaded`

--- src/io.zig
+++ src/io.zig
@@ -1,6 +1,66 @@
+const builtin = @import("builtin");
+const is_windows = builtin.os.tag == .windows;
+
+const std = @import("std");
+const Io = std.Io;
+const assert = std.debug.assert;
+
 pub const RemiellIo = @import("io/RemiellIo.zig");
 pub const MultiSocket = @import("io/MultiSocket.zig");
 
 test {
-    _ = RemiellIo;
+    std.testing.refAllDecls(@This());
+}
+
+pub fn waitForShutdownThreaded(threaded: *Io.Threaded) void {
+    const shared = struct {
+        var threaded_io: Io = undefined;
+        var event: Io.Event = .unset;
+        var once: std.atomic.Value(bool) = .init(false);
+    };
+
+    assert(!shared.once.swap(true, .monotonic)); // `waitForShutdownThreaded` was called twice.
+
+    const threaded_io = threaded.io();
+    shared.threaded_io = threaded_io;
+
+    if (is_windows) {
+        const w = std.os.windows;
+
+        const windows = struct {
+            const PHANDLER_ROUTINE = *const fn (w.DWORD) callconv(.winapi) w.BOOL;
+
+            extern "kernel32" fn SetConsoleCtrlHandler(
+                PHANDLER_ROUTINE,
+                w.BOOL,
+            ) callconv(.winapi) w.BOOL;
+
+            fn handler(_: w.DWORD) callconv(.winapi) w.BOOL {
+                shared.event.set(shared.threaded_io);
+                return .TRUE;
+            }
+        };
+
+        assert(windows.SetConsoleCtrlHandler(windows.handler, .TRUE) != .FALSE);
+    } else {
+        const system = std.posix.system;
+
+        const posix = struct {
+            fn handler(_: system.SIG) callconv(.c) void {
+                shared.event.set(shared.threaded_io);
+            }
+        };
+
+        assert(system.sigaction(
+            .INT,
+            &.{
+                .handler = .{ .handler = posix.handler },
+                .mask = std.mem.zeroes(@FieldType(system.Sigaction, "mask")),
+                .flags = 0,
+            },
+            null,
+        ) == 0);
+    }
+
+    shared.event.waitUncancelable(shared.threaded_io);
 }
